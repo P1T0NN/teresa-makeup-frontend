f3ede8dacda3540c7a4efbedb400a9c725c8e03f
P1T0NN <ognjen.tapuskovic@gmail.com>
2026-05-11 22:46:03 +0200

    Fixed bugs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,22 +2,12 @@
 	import './layout.css';
 	import favicon from '@/shared/lib/assets/favicon.svg';
 
-	// SVELTEKIT IMPORTS
-	//import { dev } from '$app/environment';
-
-	// LIBRARIES
-	//import { injectAnalytics } from '@vercel/analytics/sveltekit';
-	//import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-
 	// COMPONENTS
 	import { Toaster } from '@/shared/components/ui/sonner';
 	import Header from '@/shared/components/ui/header/header.svelte';
 	import Footer from '@/shared/components/ui/footer/footer.svelte';
 
 	let { children } = $props();
-
-	//injectAnalytics({ mode: dev ? 'development' : 'production' });
-	//injectSpeedInsights();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
